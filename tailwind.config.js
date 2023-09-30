23ae442f8d717861ed8bee093ee162113a38c933
WayuAji30 <wahyuma123@gmail.com>
2023-10-01 00:37:44 +0700

    add min and max responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                sm: { min: "375px", max: "767px" },
+                // => @media (min-width: 640px and max-width: 767px) { ... }
+
+                md: { min: "768px", max: "1023px" },
+                // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+                lg: { min: "1024px" },
+                // => @media (min-width: 1024px) { ... }
+            },
+        },
     },
     plugins: [],
 };
